3e624a5e9d3f20884fde07aad502552a58599f74
Lucasmsai <7lucas.msai7@gmail.com>
2025-01-13 21:33:17 +0100

    Change3

---
+++ front_end/src/composants/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+  <>
+  <div className='p-4 bg-white w3-display-container'>
+    <div className="w3-display-middle font-['kanit'] font-semibold ">E-Magaz</div>
+    <button className="w3-display-right pr-4 font-['kanit']">Connexion</button>
+  </div>
+
+   </>
+  )
+}
